a9ecf6772e8833f8b25f904822b29350f052fece
leticiafrontend <leticia.alesilva@outlook.com>
2022-04-01 21:23:21 -0300

    style: add path absolutos and global styles

---
+++ app/src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+@font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v12-latin-300.tff') format('tff');
+  }
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v12-latin-regular.tff') format('tff');
+  }
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v12-latin-600.tff') format('tff');
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+    html, body, #__next {
+    height: 100%;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+`
